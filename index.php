9254574616dc93956b10efe092d0ec4115d255c5
Winifred Ekwunife <winifredchineze22@gmail.com>
2018-04-09 15:08:37 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="em">
+		<head> <title>simple design</title>
+            <meta charset="utf-8"/>
+			<link rel="stylesheet" href="styles.css"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		</head>
+<body>
+    <div class="box">
+   <p>MY NAME IS WINIFRED</p><br><br><br><br><br>
+   <P>WELCOME TO HNG INTERNSHIP 4.0 </P> <br><br>
+    <p>I'M A FRONT-END DEVELOPER</p><br><br><br><br><br><br>
+    <?php 
+    $time =time();
+    $realtime =date('F jS Y h: i a');
+    
+    echo 'DATE/TIME: '.$realtime;
+    ?>
+
+    <!--<p>DATE/TIME:SUNDAY, 8TH APRIL,2018. 11:11PM.</p><br><b><br><br><br>-->
+    
+ 
+ 
+ 
+    </div>
+</body>
+
+</html>
